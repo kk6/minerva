f9bcc9b38c633919c44263d8ea8db056a3370af5
kk6 <hiro.ashiya@gmail.com>
2025-05-02 03:30:51 +0900

    refactor(server.py): write_noteを削除し、MCPのバージョンを0.2.0に更新

--- server.py
+++ server.py
@@ -3,20 +3,22 @@ import logging
 from mcp.server.fastmcp import FastMCP
 
 from minerva.tools import (
-    write_note,
     read_note,
     search_notes,
+    create_note,
+    edit_note,
 )
 
 # Set up logging
 logger = logging.getLogger(__name__)
 
 # Create an MCP server
-mcp = FastMCP("minerva", "0.1.0")
+mcp = FastMCP("minerva", "0.2.0")
 
-mcp.add_tool(write_note)
 mcp.add_tool(read_note)
 mcp.add_tool(search_notes)
+mcp.add_tool(create_note)
+mcp.add_tool(edit_note)
 
 if __name__ == "__main__":
     # Run the server
